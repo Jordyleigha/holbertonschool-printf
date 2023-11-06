762e4f1c494046f879d3073ca156d8c2a0dd26c3
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-05 17:05:02 -0800

    declaring and initializing variables used to help keep track of the function

--- testfiless/printf.c
+++ testfiless/printf.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - prints a string with format specifiers
+ * @format: the format string with format specifiers
+ *
+ * Return: number of characters in the final string
+ */
+int _printf(const char *format, ...)
+{
+	// Variable to keep track of the number of characters printed
+	int count = 0;
+	// Declare a variable to hold the list of arguments
+	va_list args;
+	// Initializing the variable with the last names parameter before '...'
