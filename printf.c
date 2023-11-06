c3d179085c5ca55731c4f893eb5ef25b6402b772
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-05 18:36:07 -0800

    making changes to our errors

--- printf.c
+++ printf.c
@@ -3,43 +3,43 @@
 #include <stdarg.h>
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list arg;
-	va_start(arg, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(arg int);
-				putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-		}
-				char *str = va_arg(arg char*);
-				while (*str != '\0')
-				{
-					putchar(*str);
-					str++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-		
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return count;
+    int count = 0;
+    va_list args;
+    va_start(args, format);
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                putchar(c);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char*);
+                while (*str != '\0')
+                {
+                    putchar(*str);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                count++;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
 }
