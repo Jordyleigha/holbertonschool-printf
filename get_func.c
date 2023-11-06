08d39120cd547f54d6c7ba83d7a70004e659cee1
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-06 13:08:24 -0800

    fixing my functions

--- get_func.c
+++ get_func.c
@@ -5,13 +5,15 @@
  * @x: variable to the function
  * Return: function
  */
-int (*get_func(char z))(va_list)
+int (*get_func(char x))(va_list)
 {
 	int i = 0;
 	spec arr[] = {
 		{"c", print_c},
 		{"s", print_s},
 		{"%", print_percent},
+		{"d", print_d},
+		{"i", print_i},
 		{NULL, NULL}
 	};
 	while (arr[i].valid)
