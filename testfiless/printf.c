6bf67df8605588c07dd96a2e0a9e93e01419d4d6
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-05 17:49:24 -0800

    making updates to help get a clear compile without error

--- testfiless/printf.c
+++ testfiless/printf.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -9,56 +10,43 @@
  */
 int _printf(const char *format, ...)
 {
-	// Variable to keep track of the number of characters printed
 	int count = 0;
-	// Declare a variable to hold the list of arguments
 	va_list args;
-	// Initializing the variable with the last names parameter before '...'
-        
-        // Loop through the format string until the end is reached ('\0')
-        while (*format != '\0')
-        {
-          if (*format == '%') // If '%' is encountered, indicating a format specifier
-          {
-              format++; // Move to the next character after '%'
-              if (*format == 'c') // If the format specifier is '%c' (character)
-              {
-                  char c = va_arg(args, int); // Get the character argument from the list
-                  putchar(c); // Print the character
-                  count++; // Increment the character count
-              }
-	      //If the format specifier is '%s'(string)
-	      else if(*format == 's')
-	  }
-	  //getting the string argument from the list
-	  char *str = va_args(args, char*);
-	  //Loop through the characters of the string
-	  while (*str != '\0')
-	  {
-		  //print each character of the string
-		  putchar(*str);
-		  str++;
-		  //Increment the character count
-		  count++;
-	  }
-	}
-	// If the format specifier is '%%'
-	else if (*format == '%')
+	va_start(args, format);
+	while (*format != '\0')
 	{
-		//print the percent sign
-		putchar('%');
-		//Increment the character count
-		count++;
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
 	}
-        }
-            else if (*format == '%') // If the format specifier is '%%' (percent symbol)
-            {
-                putchar('%'); // Print '%' character
-                count++; // Increment the character count
-            }
-            format++; // Move to the next character in the format string
-    }
-
-    va_end(args); // Clean up the variable holding the arguments
-    return count; // Return the total number of characters printed
+	va_end(args);
+	return count;
 }
