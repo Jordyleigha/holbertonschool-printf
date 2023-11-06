41ecdd7edfd2ac9c7bbe886d2d7f1fa269c86af3
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-05 19:32:00 -0600

    Update main.h

--- testfiless/main.h
+++ testfiless/main.h
@@ -8,3 +8,10 @@ typedef struct {
     char spec;  /* Format specifier character ('c', 's', or '%') */
     int (*func)(va_list);  /* Function pointer for the corresponding print function */
 } FormatSpecifier;
+
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
+#endif /* MAIN_H */
