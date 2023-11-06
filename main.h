d10301e74c1da01e93a796f8907c4f359ca91b1f
Jordan Arguelles <jordan.arguelles.ja@gmail.com>
2023-11-06 12:51:46 -0800

    updating the header file

--- main.h
+++ main.h
@@ -1,18 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 #include <stdarg.h>
-
-/* Custom data structure to represent the format specifier */
-typedef struct {
-    char spec;  /* Format specifier character ('c', 's', or '%') */
-    int (*func)(va_list);  /* Function pointer for the corresponding print function */
-} FormatSpecifier;
-
+/**
+ * struct specifier - struct specifier
+ * @valid: valid character
+ * @f: the functions associated
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
+int print_c(va_list args);
+int print_s(va_list args);
 int print_percent(va_list args);
 int _putchar(char c);
-
-#endif /* MAIN_H */
+#endif
