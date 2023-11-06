a6646c96f4e0c016d1b38ce88eed6ee61d21a599
root <ivonnexlopez@gmail.com>
2023-11-05 17:56:59 -0800

    3d

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/* Custom data structure to represent the format specifier */
+typedef struct {
+    char spec;  /* Format specifier character ('c', 's', or '%') */
+    int (*func)(va_list);  /* Function pointer for the corresponding print function */
+} FormatSpecifier;
+
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
+#endif /* MAIN_H */
+
